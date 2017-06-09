5e30e9e1b9900f4bc04ad6e74409bf9c2b4fadf1
igaozp <igaozp@hotmail.com>
2017-06-09 14:35:21 +0800

    Add vue-resource support

--- src/main.js
+++ src/main.js
@@ -3,11 +3,13 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import VueResource from 'vue-resource'
 import MuseUI from 'muse-ui'
 import 'muse-ui/dist/muse-ui.css'
 
 Vue.config.productionTip = false
 Vue.use(MuseUI)
+Vue.use(VueResource)
 
 /* eslint-disable no-new */
 new Vue({
