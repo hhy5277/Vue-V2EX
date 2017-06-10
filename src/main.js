27709ba3035465dbf535df431f3d81a41dcab63f
igaozp <igaozp@hotmail.com>
2017-06-10 15:15:16 +0800

    Add axios support

--- src/main.js
+++ src/main.js
@@ -3,13 +3,14 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import VueResource from 'vue-resource'
+import Axios from 'axios'
+import VueAxios from 'vue-axios'
 import MuseUI from 'muse-ui'
 import 'muse-ui/dist/muse-ui.css'
 
 Vue.config.productionTip = false
 Vue.use(MuseUI)
-Vue.use(VueResource)
+Vue.use(Axios, VueAxios)
 
 /* eslint-disable no-new */
 new Vue({
