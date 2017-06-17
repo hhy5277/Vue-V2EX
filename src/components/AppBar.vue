dff0a607b45c083f57dd738752bee916d2f9030d
igaozp <igaozp@hotmail.com>
2017-06-17 17:41:11 +0800

    Refactor

--- src/components/AppBar.vue
+++ src/components/AppBar.vue
@@ -1,128 +1,158 @@
 <template>
-<div>
-  <div class="app-bar">
-    <mu-appbar title="V2EX">
-      <mu-icon-button icon="menu" slot="left" @click="toggle(true)"/>
-      <mu-icon-button icon="expand_more" slot="right"/>
-      <mu-drawer :open="open" :docked="docked" @close="toggle()">
-        <mu-list @itemClick="docked ? '' : toggle()">
-          <mu-list-item title="Item 1"/>
-          <mu-list-item title="Item 2"/>
-          <mu-list-item v-if="docked" @click.native="open = false" title="Close"/>
-        </mu-list>
-      </mu-drawer>
-    </mu-appbar>
-  </div>
-  <div class="app-tabs">
-    <mu-tabs :value="activeTab" @change="handleTabChange">
-      <mu-tab value="tab1" title="全部"/>
-      <mu-tab value="tab2" title="最热"/>
-      <mu-tab value="tab3" title="技术"/>
-      <mu-tab value="tab4" title="创意"/>
-      <mu-tab value="tab5" title="好玩"/>
-      <mu-tab value="tab6" title="Apple"/>
-      <mu-tab value="tab7" title="酷工作"/>
-      <mu-tab value="tab8" title="交易"/>
-      <mu-tab value="tab9" title="城市"/>
-      <mu-tab value="tab10" title="问与答"/>
-      <mu-tab value="tab11" title="R2"/>
-      <mu-tab value="tab12" title="节点"/>
-    </mu-tabs>
-    <div v-if="activeTab === 'tab1'">
-      <h2>全部</h2>
-      <p>这是第1个 tab</p>
+  <div>
+    <div class="app-bar">
+      <mu-appbar title="V2EX">
+        <mu-icon-button icon="menu" slot="left" @click="toggle(true)"></mu-icon-button>
+        <mu-icon-button icon="expand_more" slot="right"></mu-icon-button>
+        <mu-drawer :open="open" :docked="docked" @close="toggle()">
+          <mu-list @itemClick="docked ? '' : toggle()">
+            <mu-list-item title="Item 1"></mu-list-item>
+            <mu-list-item title="Item 2"></mu-list-item>
+            <mu-list-item v-if="docked" @click.native="open = false" title="Close"></mu-list-item>
+          </mu-list>
+        </mu-drawer>
+      </mu-appbar>
     </div>
-    <div v-if="activeTab === 'tab2'">
-      <h2>最热</h2>
-      <p>这是第2个 tab</p>
-    </div>
-    <div v-if="activeTab === 'tab3'">
-      <h2>技术</h2>
-      <p>这是第3个 tab</p>
-    </div>
-    <div v-if="activeTab === 'tab4'">
-      <h2>创意</h2>
-      <p>这是第4个 tab</p>
-    </div>
-    <div v-if="activeTab === 'tab5'">
-      <h2>好玩</h2>
-      <p>这是第5个 tab</p>
-    </div>
-    <div v-if="activeTab === 'tab6'">
-      <h2>Apple</h2>
-      <p>这是第6个 tab</p>
-    </div>
-    <div v-if="activeTab === 'tab7'">
-      <h2>酷工作</h2>
-      <p>这是第7个 tab</p>
-    </div>
-    <div v-if="activeTab === 'tab8'">
-      <h2>交易</h2>
-      <p>这是第8个 tab</p>
-    </div>
-    <div v-if="activeTab === 'tab9'">
-      <h2>城市</h2>
-      <p>这是第9个 tab</p>
-    </div>
-    <div v-if="activeTab === 'tab10'">
-      <h2>问与答</h2>
-      <p>这是第10个 tab</p>
-    </div>
-    <div v-if="activeTab === 'tab11'">
-      <h2>R2</h2>
-      <p>这是第11个 tab</p>
-    </div>
-    <div v-if="activeTab === 'tab12'">
-      <h2>节点</h2>
-      <p>这是第12个 tab</p>
+    <div class="app-tabs">
+      <mu-tabs :value="activeTab" @change="handleTabChange">
+        <mu-tab value="all" title="全部" @click="processTarget('all')"></mu-tab>
+        <mu-tab value="hot" title="最热" @click="processTarget('hot')"></mu-tab>
+        <mu-tab value="tech" title="技术" @click="processTarget('tech')"></mu-tab>
+        <mu-tab value="idea" title="创意" @click="processTarget('idea')"></mu-tab>
+        <mu-tab value="fun" title="好玩" @click="processTarget('fun')"></mu-tab>
+        <mu-tab value="apple" title="Apple" @click="processTarget('apple')"></mu-tab>
+        <mu-tab value="job" title="酷工作" @click="processTarget('job')"></mu-tab>
+        <mu-tab value="deal" title="交易" @click="processTarget('deal')"></mu-tab>
+        <mu-tab value="city" title="城市" @click="processTarget('city')"></mu-tab>
+        <mu-tab value="ask" title="问与答" @click="processTarget('ask')"></mu-tab>
+        <mu-tab value="r2" title="R2" @click="processTarget('r2')"></mu-tab>
+        <mu-tab value="node" title="节点" @click="processTarget('node')"></mu-tab>
+      </mu-tabs>
+      <div v-if="activeTab === 'all'">
+        <h2>全部</h2>
+      </div>
+      <div v-if="activeTab === 'hot'">
+        <h2>最热</h2>
+      </div>
+      <div v-if="activeTab === 'tech'">
+        <h2>技术</h2>
+      </div>
+      <div v-if="activeTab === 'idea'">
+        <h2>创意</h2>
+      </div>
+      <div v-if="activeTab === 'fun'">
+        <h2>好玩</h2>
+      </div>
+      <div v-if="activeTab === 'apple'">
+        <h2>Apple</h2>
+      </div>
+      <div v-if="activeTab === 'job'">
+        <h2>酷工作</h2>
+      </div>
+      <div v-if="activeTab === 'deal'">
+        <h2>交易</h2>
+      </div>
+      <div v-if="activeTab === 'city'">
+        <h2>城市</h2>
+      </div>
+      <div v-if="activeTab === 'ask'">
+        <h2>问与答</h2>
+      </div>
+      <div v-if="activeTab === 'r2'">
+        <h2>R2</h2>
+      </div>
+      <div v-if="activeTab === 'node'">
+        <h2>节点</h2>
+      </div>
     </div>
   </div>
-</div>
 </template>
 
 <script>
-export default {
-  name: 'hello',
-  data () {
-    return {
-      activeTab: 'tab1',
-      open: false,
-      docked: true,
-      articles: null
-    }
-  },
-  methods: {
-    handleTabChange (val) {
-      this.activeTab = val
+  export default {
+    name: 'hello',
+    data () {
+      return {
+        activeTab: 'all',
+        open: false,
+        docked: true,
+        articles: null
+      }
     },
-    toggle (flag) {
-      this.open = !this.open
-      this.docked = !flag
+    methods: {
+      handleTabChange (val) {
+        this.activeTab = val
+        if (val === 'hot') {
+          this.$store.dispatch('getHot')
+        }
+      },
+      toggle (flag) {
+        this.open = !this.open
+        this.docked = !flag
+      },
+      processTarget (type) {
+        switch (type) {
+          case 'all':
+            console.log('all')
+            break
+          case 'hot':
+            console.log('hot')
+            break
+          case 'tech':
+            console.log('tech')
+            break
+          case 'idea':
+            console.log('idea')
+            break
+          case 'fun':
+            console.log('fun')
+            break
+          case 'apple':
+            console.log('apple')
+            break
+          case 'job':
+            console.log('job')
+            break
+          case 'deal':
+            console.log('deal')
+            break
+          case 'city':
+            console.log('city')
+            break
+          case 'ask':
+            console.log('ask')
+            break
+          case 'r2':
+            console.log('r2')
+            break
+          case 'node':
+            console.log('node')
+            break
+          default:
+            console.log('nothing')
+        }
+      }
     }
-  },
-  created () {
-    this.$store.dispatch('getHot')
   }
-}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
+  h1, h2 {
+    font-weight: normal;
+  }
 
-ul {
-  list-style-type: none;
-  padding: 0;
-}
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
 
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
 
-a {
-  color: #42b983;
-}
+  a {
+    color: #42b983;
+  }
 </style>
